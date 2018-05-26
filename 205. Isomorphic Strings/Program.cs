120a7eb81995bd52a89f17ba3741466e321b8791
jsw7524 <jsw7524@gmail.com>
2018-05-26 21:01:51 +0800

    Add project files.

---
+++ 205. Isomorphic Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _205.Isomorphic_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
