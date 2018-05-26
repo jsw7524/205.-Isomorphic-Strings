0dfa6d43a74624791b56eb8a6d97e3e1c0beae95
jsw7524 <jsw7524@gmail.com>
2018-05-26 23:05:04 +0800

    AC

--- 205. Isomorphic Strings/Program.cs
+++ 205. Isomorphic Strings/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace _205.Isomorphic_Strings
 {
+
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            return IsomorphicChecker(s,t) && IsomorphicChecker(t,s);
+        }
+
+        private bool IsomorphicChecker(string s, string t)
+        {
+            var dic = new Dictionary<char, char>();
+            foreach (var c in Enumerable.Zip(Enumerable.Range(0, s.Length), s, (a, b) => new { Index = a, Character = b }))
+            {
+                if (!dic.ContainsKey(c.Character))
+                {
+                    dic.Add(c.Character, t[c.Index]);
+                }
+                else
+                {
+                    if (t[c.Index] != dic[c.Character])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using _205.Isomorphic_Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new Solution();
+            var result = solution.IsIsomorphic("egg","add");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var solution = new Solution();
+            var result = solution.IsIsomorphic("foo", "bar");
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var solution = new Solution();
+            var result = solution.IsIsomorphic("paper", "title");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var solution = new Solution();
+            var result = solution.IsIsomorphic("aa", "ab");
+            Assert.IsFalse(result);
+        }
+    }
+}
